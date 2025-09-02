d821d0c9541bfd4223802b59221f38bc3a52aa2e
frammenti <fra.mmenti@inventati.org>
2025-09-02 09:56:29 +0200

    Use hgroup instead of .subtitle

--- src/sections/LinguisticFeatures.vue
+++ src/sections/LinguisticFeatures.vue
@@ -1,6 +1,8 @@
 <template>
-  <section id='linguistic-features' aria-label='Linguistic features'>
+<section id='linguistic-features' aria-label='Linguistic features'>
+  <hgroup>
     <h2>Linguistic features</h2>
-    <p>Valence, arousal etc.</p>
-  </section>
+  </hgroup>
+  <p>Valence, arousal etc.</p>
+</section>
 </template>
