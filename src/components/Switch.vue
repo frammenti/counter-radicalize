5c02e3c3046003989f4702908970c5a341eb1983
frammenti <fra.mmenti@inventati.org>
2025-09-25 08:51:06 +0200

    Responsive switch size

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -21,42 +21,43 @@ const state = defineModel<boolean>('modelValue')
 <style scoped lang='scss'>
 .input-container {
   display: flex;
-  flex-flow: row-reverse nowrap;
+  flex-flow: row nowrap;
   align-items: center;
   justify-content: start;
-  gap: 0.5rem;
-  font-size: 0.9rem;
+  gap: 0.5em;
 }
 label {
   flex-shrink: 2;
   transition: all 150ms cubic-bezier(.4,0,.2,1);
+  font-size: 0.9em;
 }
 .button {
   display: block;
   padding: 0;
-  width: 2.25rem;
-  height: 1.25rem;
-  background-color: resp((shadow($brown, 0.2), $black));
+  width: 2.25em;
+  height: 1.25em;
+  background-color: resp((shadow($brown, 0.2), shadow($lilac, 0.2)));
   border: 0.5px solid resp($border-color);
   transition: all 150ms cubic-bezier(.4,0,.2,1);
   will-change: background-color;
   position: relative;
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  box-shadow: none;
 }
 .button[data-state='checked'] {
   background-color: resp((shadow($orchid, 0.6), $taupe));
 }
 .thumb {
   display: block;
-  width: 1rem;
-  height: 1rem;
+  width: 1em;
+  height: 1em;
   background-color: $white;
   border-radius: 9999px;
   box-shadow: 0 2px 2px resp($shadow-color);
   transition: transform 100ms;
   position: absolute;
-  top: 0.1rem;
-  inset-inline-start: 0.1rem;
+  top: 0.075em;
+  inset-inline-start: 0.1em;
   will-change: transform;
 }
 .thumb[data-state='checked'] {
